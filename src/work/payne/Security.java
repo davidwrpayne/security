fd3af2b86b98ed4575dff268e9712454c1fd503b
David Payne <david.payne@bigcommerce.com>
2016-03-17 14:43:34 -0700

    initial Class and pom file

---
+++ src/work/payne/Security.java
@@ -0,0 +1,12 @@
+package work.payne;
+
+/**
+ * Created by david.payne on 3/17/16.
+ */
+public class Security {
+
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
